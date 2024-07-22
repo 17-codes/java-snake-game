5a0f0c5420de05ce53a11b948bf2e1c082fc591b
ash <as22h23thi@gmail.com>
2024-07-22 23:32:38 +0530

    added game  window

--- src/App.java
+++ src/App.java
@@ -1,4 +1,16 @@
+import javax.swing.*;;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        int gameWidth = 700;
+        int gameHeight = 700;
+
+        JFrame frame = new JFrame("snake");
+        frame.setSize(gameWidth, gameHeight); // Set the size of the JFrame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Exit application on close
+        frame.setResizable(false); // Disable resizing of the JFrame
+        frame.setLocationRelativeTo(null); // Center the JFrame on the screen
+        frame.setVisible(true); // Make the JFrame visible
+
     }
 }
